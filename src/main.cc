a671497dc6e687a4eda3f4f98e642f538b79b49d
Sebastian Käwel <sebastian.kaewel@stud.thga.de>
2022-12-08 21:22:36 +0100

    ende Vorlesung

--- src/main.cc
+++ src/main.cc
@@ -6,8 +6,8 @@ int main(int argc, char** argv)
 	std::cout << "Hello World" << "\n";
 
 	CLI::App app{"Dies ist ein Programm was uns den Umgang mit Lagerhaltungsdaten zeigen soll","Lagerhaltungsprogramm"};
-    int flag_int{0};
-    app.add_option("-i",flag_int,"int");
+    int option_int{0};
+    app.add_option("-i",option_int,"int");
 
     bool flag_bool{false};
     app.add_flag("-b", flag_bool, "bool");
@@ -18,8 +18,8 @@ int main(int argc, char** argv)
         return app.exit(e);
     }
 
-    if(flag_int >0)
-        std::cout <<"Flag : "<< flag_int <<std::endl;
+    if(option_int >0)
+        std::cout <<"Option Int : "<< option_int <<std::endl;
 
     if(flag_bool)
         std::cout <<"Bool erkannt : "<<flag_bool <<std::endl;
