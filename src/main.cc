9e49068710804baa89b460df86bf6ff369a9211c
Sebastian Käwel <sebastian.kaewel@stud.thga.de>
2023-01-09 22:08:40 +0100

    Nach Vorlesung

--- src/main.cc
+++ src/main.cc
@@ -5,11 +5,8 @@
 #include <string>
 
 using json = nlohmann::json;
-//nlohmann::json database_object;
-//                                          zum starten       ./build/main.cc -r src/lagerhalterung.json      ausgeben lassen! 
+ 
 struct statemachine {
-    //statemachine(json& database_object);
-    //    : mein_json{database_object};
     json& mein_json;
 
     statemachine(json& database_object)
@@ -17,66 +14,57 @@ struct statemachine {
         std::cout << "Konstruktor called with Initilizerlist!\n";
     }
 
-    void aendern() {
-
+    void aendern() 
+    {
+        //bevor Daten geändert werden sollen die Inhalte Ausgegeben werden
         ausgeben();
 
-        //for (auto& change : mein_json["Regale"])
-        //{
-            int regalnummer; //Integer für die Options
+        int regalnummer; //Integer für die Options
+        std::string neuerinhalt,alterinhalt,wahl;
 
-            std::string neuerinhalt,alterinhalt,wahl;
+        std::cout << "Welches Regal soll geändert werden?" << "\n";
+        std::cin>> regalnummer;
 
-
-             
-
-            std::cout << "Welches Regal soll geändert werden?" << "\n";
-
-            std::cin>> regalnummer;
-
-            
-
-            for (auto& change : mein_json["Regale"])
-            {
-             if(change ["Regal"] == regalnummer)
-             {  
-
-            
-            std::cout << change["Inhalt"] << "\n"; 
-            std::cout << "Welcher Inhalt soll geändert werden?" << "\n"; 
-            std::cin>> alterinhalt;
-            for (auto& aktuellinhalt : change["Inhalt"] ){
+        for (auto& change : mein_json["Regale"])
+        {
+            if(change ["Regal"] == regalnummer)
+            {  
+                std::cout << change["Inhalt"] << "\n"; 
+                std::cout << "Welcher Inhalt soll geändert werden?" << "\n"; 
+                std::cin>> alterinhalt;
+                for (auto& aktuellinhalt : change["Inhalt"] )
+                {
                     if (aktuellinhalt==alterinhalt)
-                        {   
-                         std::cout << aktuellinhalt << "\n";         
-                         std::cout << "Neuer Wert" << "\n"; 
-                         std::cin >> neuerinhalt;
-                        aktuellinhalt =neuerinhalt;
+                    {   
+                        std::cout << aktuellinhalt << "\n";         
+                        std::cout << "Neuer Wert" << "\n"; 
+                        std::cin >> neuerinhalt;
+                        aktuellinhalt=neuerinhalt; // Wenn direkt in Variable geschriebenwird, dann gibt es einen fehler beim parsen
+                        //Hier erfolgt ein Early-Return wenn die Arbeit erfolgreich abgeschlossen wurde
                         return;
-                         }
-                                                
-                        
-                        }
-                        std::cout <<"Leider wurde der Inhalt " << alterinhalt <<" nicht gefunden!!! Soll er hinzugefügt werden?? [J/N]" << "\n";
-                        std::cin >> wahl;
-                            if (wahl == "J")
-                            {
-                                    change ["Inhalt"].push_back(alterinhalt);
-                                    break;
-                            }
-                            else
-                            {
-                                std::cout << "Dann eben nicht!" << "\n";
-                                break;
-                            }
-                           
+                    }
+                }
+
+                //Wird nur Ausgeführt, wenn der zu verändernde Inhalt nicht gefunden wird
+                std::cout <<"Leider wurde der Inhalt " << alterinhalt <<" nicht gefunden!!! Soll er hinzugefügt werden?? [J/N]" << "\n";
+                std::cin >> wahl;
+
+                if (wahl == "J")
+                {
+                    change ["Inhalt"].push_back(alterinhalt);
+                    break;
+                }
+                else
+                {
+                    std::cout << "Dann eben nicht!" << "\n";
+                    break;
+                }
             }
-            }
-            
-      
+        }
     }
 
-    void ausgeben() {
+    void ausgeben() 
+    {
         for (auto& element : mein_json["Regale"])
         {
             int belegte_plaetze, anzahl_plaetze, leere_plaetze;
@@ -98,36 +86,37 @@ struct statemachine {
         }
     }
 
-    void speichern() {
+    void speichern() 
+    {
         std::string speicherpfad{};
-
         std::cout << "Name der Datei: ";
-
         std::cin >> speicherpfad;
 
-        std::cout << "\nSpeicherpfad: /CLI11/" << speicherpfad << std::endl;    //Optional. Gibt nur im Terminal den Speicherpfad aus, damit es "schön" aussieht!
+        //Optional. Gibt nur im Terminal den Speicherpfad aus, damit es "schön" aussieht!
+        std::cout << "\nSpeicherpfad: /CLI11/" << speicherpfad << std::endl;    
         //Dieser muss je nach dem geändert werden, sollte euer Ordner "wo anders liegen"
-        std::ofstream save_as{speicherpfad};                                    //bzw. sollte die Datei "anders heißen"
+        //bzw. sollte die Datei "anders heißen"
         //Beispiel: "\nSpeicherpfad: Server/maxmustermann/Projekte/Lagersysteme/" usw.
+        std::ofstream save_as{speicherpfad};
+
         save_as << mein_json.dump(4);
-        //Warum auch immer wird die Reihenfolge bei neuem Speichern nicht eingehalten --Gerne Lösungen dazu Teilen!
+        //Warum auch immer wird die Reihenfolge bei neuem Speichern nicht eingehalten
+        //Gerne Lösungen dazu Teilen!
         save_as.close();
     }
 };
 
-int main(int argc, char** argv) {
-
-    nlohmann::json database_object;
-
-    std::cout << "Moin! Hier ist ein Lagerhalterungssystem" << "\n" << std::endl;
-
+int main(int argc, char** argv) 
+{
+    json database_object;
+    std::string filepath;
+    
     CLI::App app{"Schreibe: -r src/lagerhalterung.json \n"};
-
-    std::string filepath; //Strings für die Options
-
     app.add_option("-r,--read", filepath, "Path to config file")
     ->required()
     ->check(CLI::ExistingFile);
+    
+    std::cout << "Moin! Hier ist ein Lagerhalterungssystem" << std::endl;
 
     //try & catch Funktion, sollte ein Parse Error auftreten, so wird die "app" geschlossen
     try
@@ -140,7 +129,6 @@ int main(int argc, char** argv) {
     }
 
     std::ifstream file{filepath};
-
     if(!file.is_open())
     {
         std::cout << "Error opening file!\n" << std::endl;
@@ -155,19 +143,14 @@ int main(int argc, char** argv) {
     catch (nlohmann::json::parse_error& ex)
     {
         std::cerr << "parse error at byte " << ex.byte << std::endl;
-
         std::exit(EXIT_FAILURE);
     }
 
-    //statemachine lagerhaltung_statemachine();
     statemachine lagerhaltung_statemachine{database_object};
 
     while(true)
     {
-        std::cout << "Hallo und Willkommen in unserem Lagersytem!" << std::endl;
-
         int aktueller_state = 0;
-
         while((aktueller_state < 1) || (aktueller_state > 4 ))
         {
             std::cout <<    "Du kannst wählen zwischen: \n"
@@ -183,19 +166,19 @@ int main(int argc, char** argv) {
             }
         }
 
-        if(aktueller_state == 1)//Funktion/State Inhalt Ändern
+        if(aktueller_state == 1)
         {
             lagerhaltung_statemachine.aendern();
         }
-        else if(aktueller_state == 2)//Funktion/State Inhalt Ausgeben
+        else if(aktueller_state == 2)
         {
             lagerhaltung_statemachine.ausgeben();
         }
-        else if(aktueller_state == 3)//Funktion/State Inhalt Speichern
+        else if(aktueller_state == 3)
         {
             lagerhaltung_statemachine.speichern();
         }
-        else if(aktueller_state == 4)//Funktion/State Programmende
+        else if(aktueller_state == 4)
         {
             break;
         }
